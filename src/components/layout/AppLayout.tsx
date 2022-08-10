d5325edebe52c99dcb363bfd1393d0427ee893c1
Daniel Castillejo <devcastillejo@gmail.com>
2022-08-09 20:11:27 -0500

    Agrega menu navegacion

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -1,6 +1,8 @@
 import React from "react"
-import { GiftIcon, HomeIcon } from "@heroicons/react/outline"
 import Link from "next/link"
+import { User } from "@prisma/client"
+import { useSession } from "next-auth/react"
+import { GiftIcon, HomeIcon, UserGroupIcon } from "@heroicons/react/outline"
 
 type AppLayoutProps = {
   children: React.ReactNode
@@ -12,26 +14,36 @@ type TabLinkProps = {
   icon: JSX.Element
 }
 
-const navigation = [
-  { name: "Inicio", href: "/app/home", icon: HomeIcon },
-  { name: "Premios", href: "/app/awards", icon: GiftIcon }
-]
+type TabUserProps = {
+  href: string
+  user: User | undefined
+}
 
 const TabBarMenu = (): JSX.Element => {
+  const { data: session, status } = useSession()
+
+  if (status === "loading") {
+    ;<span>Cargando...</span>
+  }
+
   return (
-    <nav>
-      <div className="flex justify-between px-4 py-2">
-        <TabLink
-          name="Inicio"
-          href="/app/home"
-          icon={<HomeIcon className="text-current" />}
-        />
-        <TabLink
-          name="Premios"
-          href="/app/awards"
-          icon={<GiftIcon className="text-current" />}
-        />
-      </div>
+    <nav className="flex justify-between bg-black px-6 py-8">
+      <TabLink
+        name="Inicio"
+        href="/app/home"
+        icon={<HomeIcon className="text-current" />}
+      />
+      <TabLink
+        name="Premios"
+        href="/app/awards"
+        icon={<GiftIcon className="text-current" />}
+      />
+      <TabLink
+        name="Familia"
+        href="/app/family"
+        icon={<UserGroupIcon className="text-current" />}
+      />
+      <TabUser href="/app/me" user={session?.user as User} />
     </nav>
   )
 }
@@ -41,7 +53,23 @@ const TabLink = ({ href, name, icon }: TabLinkProps): JSX.Element => {
     <Link href={href}>
       <a className="text-gray-500 hover:text-blue-500">
         <div className="h-8 w-8">{icon}</div>
-        {name}
+        {/* {name} */}
+      </a>
+    </Link>
+  )
+}
+
+const TabUser = ({ href, user }: TabUserProps): JSX.Element => {
+  return (
+    <Link href={href}>
+      <a className="text-gray-500 hover:text-blue-500">
+        {user?.image ? (
+          <div className="h-8 w-8 overflow-hidden rounded-full bg-gray-400">
+            <img src={user.image} alt="Avatar" />
+          </div>
+        ) : (
+          <div className="h-8 w-8 rounded-full bg-gray-400"></div>
+        )}
       </a>
     </Link>
   )
