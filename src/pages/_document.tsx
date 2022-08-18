2058070114f6f46faf8356d20b089b26aff9c428
Daniel Castillejo <devcastillejo@gmail.com>
2022-08-18 15:20:04 -0500

    Cambios menores a despliegue

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,7 +11,7 @@ class MyDocument extends Document {
           />
           <meta name="mobile-web-app-capable" content="yes" />
           <meta name="apple-mobile-web-app-capable" content="yes"></meta>
-          <meta name="theme-color" content="#171717"></meta>
+          <meta name="theme-color" content="#1E1E1E"></meta>
           <link rel="manifest" href="/manifest.json" />
           <link
             rel="apple-touch-icon"
