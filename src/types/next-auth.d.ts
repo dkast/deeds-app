c6bd38627e8a78acb15d7eabdba87f9cf0d5e620
Daniel Castillejo <devcastillejo@gmail.com>
2022-09-10 16:37:16 -0500

    Corrge type de next-auth

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -1,4 +1,6 @@
-import { DefaultSession } from "next-auth"
+import { DefaultSession, Session } from "next-auth"
+import type { NextComponentType, NextPageContext } from "next"
+import type { Router } from "next/router"
 
 declare module "next-auth" {
   /**
@@ -15,5 +17,18 @@ declare module "next-auth" {
   interface User {
     role: Role
     familyId: string
+  }
+}
+
+declare module "next/app" {
+  type AppProps<P = Record<string, unknown>> = {
+    Component: NextComponentType<NextPageContext, any, P>
+    router: Router
+    __N_SSG?: boolean
+    __N_SSP?: boolean
+    pageProps: P & {
+      /** Initial session passed in from `getServerSideProps` or `getInitialProps` */
+      session?: Session
+    }
   }
 }
