ca5ab8938fe9bf1b2b2d0d735a5d24dabbc81556
Daniel Castillejo <devcastillejo@gmail.com>
2022-10-19 20:36:39 -0500

    Cambia layout para trabajar mejor en movil

--- src/components/ExchangeSheet.tsx
+++ src/components/ExchangeSheet.tsx
@@ -6,6 +6,7 @@ import Input from "@/components/Input"
 import Button from "@/components/Button"
 import { trpc } from "@/src/utils/trpc"
 import AwardView from "@/components/AwardView"
+import { Award } from "@prisma/client"
 
 type ExchangeSheetProps = {
   open: boolean
@@ -56,9 +57,9 @@ const ExchangeSheet = ({ open, setOpen, userId }: ExchangeSheetProps) => {
             <div className="flex flex-col gap-2">
               {awards?.map(award => {
                 return (
-                  <motion.div key={award.id} whileTap={{ scale: 0.95 }}>
-                    <AwardView item={award} allowDelete={false}></AwardView>
-                  </motion.div>
+                  <div key={award.id} className="select-none">
+                    <ExchangeAwardItem item={award}></ExchangeAwardItem>
+                  </div>
                 )
               })}
             </div>
@@ -70,3 +71,33 @@ const ExchangeSheet = ({ open, setOpen, userId }: ExchangeSheetProps) => {
 }
 
 export default ExchangeSheet
+
+type ExchangeAwardItemProps = {
+  item: Award
+}
+
+const ExchangeAwardItem = ({ item }: ExchangeAwardItemProps) => {
+  return (
+    <div className="z-10 flex overflow-hidden rounded-lg bg-neutral-700 shadow-lg">
+      <img
+        draggable="false"
+        src={item.imageUrl}
+        className="h-36 w-36 object-cover"
+      />
+      <div className="flex w-full flex-col items-start justify-between gap-2 p-4">
+        <div className="flex w-full items-center justify-between">
+          <span className="text-xl font-bold text-gray-200">
+            {item.description}
+          </span>
+          <div className="flex items-center rounded-full bg-neutral-500 px-2 py-1 text-white">
+            <img src="../images/gem.svg" className="mr-1 h-4 w-4" alt="coin" />
+            <span className="text-sm font-bold">{item.points}</span>
+          </div>
+        </div>
+        <Button type="button" variant="primary" size="sm">
+          Canjear
+        </Button>
+      </div>
+    </div>
+  )
+}
