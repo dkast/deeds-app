7c264046e3b6e36f073874b838e22d3390d087d8
Daniel Castillejo <dcastillejo@rassini.com>
2023-06-28 20:22:07 -0500

    Migra pantalla inical a AppDir

---
+++ src/app/page.tsx
@@ -0,0 +1,28 @@
+import LottiePlayer from "@/src/components/shared/lottie-player"
+import Link from "next/link"
+
+import lottieJSON from "../../public/assets/robot-playing.json"
+
+export default function Page() {
+  return (
+    <div className="flex flex-col h-screen items-center justify-center gap-10 bg-gradient-to-b from-neutral-900 to-violet-900">
+      <div className="relative mb-16 gap-2">
+        <h1 className="text-4xl font-semibold text-violet-300">Deberes</h1>
+        <div className="absolute top-0 -right-10">
+          <small className="rounded-full bg-cyan-500 px-2 text-sm font-semibold tracking-tight text-cyan-900">
+            v3
+          </small>
+        </div>
+      </div>
+      <div>
+        <LottiePlayer lottieJSON={lottieJSON} />
+      </div>
+      <Link
+        href="/home"
+        className="flex items-center gap-4 rounded-xl bg-gradient-to-r from-cyan-500 via-violet-500 to-orange-500 px-4 py-2 text-2xl text-white"
+      >
+        Entrar
+      </Link>
+    </div>
+  )
+}
