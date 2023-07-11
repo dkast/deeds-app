04ed19b494ddba2d472a104b7f3d22c85ea0f7e8
Daniel Castillejo <devcastillejo@gmail.com>
2023-07-10 20:05:13 -0500

    Agrega pagina Familia

---
+++ src/app/(application)/family/page.tsx
@@ -0,0 +1,12 @@
+import NavBar from "@/components/layout/nav-bar"
+
+export default function FamilyPage() {
+  return (
+    <>
+      <NavBar title="Familia" />
+      <div className="mt-20 mb-28">
+        <h1>Family Page</h1>
+      </div>
+    </>
+  )
+}
