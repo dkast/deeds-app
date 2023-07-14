ecf0cb93cf3fc322fe7409a95f0fd350d8dd093c
Daniel Castillejo <devcastillejo@gmail.com>
2023-07-13 19:46:35 -0500

    Prepara componente para crear deeds

--- src/app/(application)/new/page.tsx
+++ src/app/(application)/new/page.tsx
@@ -1,10 +1,9 @@
 import { redirect } from "next/navigation"
 
-import ActivityButton from "@/components/activyt-button"
+import CreateDeed from "@/components/create-deed"
 import NavBar from "@/components/layout/nav-bar"
 import { authOptions } from "@/lib/auth"
 import { getCurrentUser } from "@/lib/session"
-import { ACTIVITIES } from "@/lib/types"
 
 export default async function NewPage() {
   const user = await getCurrentUser()
@@ -16,22 +15,7 @@ export default async function NewPage() {
   return (
     <>
       <NavBar title="Agregar" />
-      <div className="mt-20 mb-28 flex flex-col items-center">
-        <div>
-          <ul className="grid grid-cols-2 gap-2">
-            {ACTIVITIES.map(act => (
-              <li key={act.id}>
-                <ActivityButton
-                  iconName={act.icon}
-                  text={act.description}
-                  points={act.points}
-                  // onClick={() => onActivityTap(act)}
-                ></ActivityButton>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      <CreateDeed />
     </>
   )
 }
